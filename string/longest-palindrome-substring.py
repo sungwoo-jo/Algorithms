b911728b7d2aadb3a23e544d276f20fe3876f2b5
sungwoo-jo <sungwoo9671@naver.com>
2022-08-24 13:22:00 +0900

    longest-palindrome-substring

--- string/longest-palindrome-substring.py
+++ string/longest-palindrome-substring.py
@@ -5,8 +5,9 @@
 # output: "bb"
 
 def longestPalindrome(s: str) -> str:
+	# 팰린드롬 판별 및 투 포인터 확장
     def expand(left: int, right: int) -> str:
-        while left >= 0 and right < len(s) and s[left] == s[right]:
+        while left >= 0 and right < len(s) and s[left] == s[right]: # 확장을 하는 조건
             left -= 1
             right += 1
         return s[left + 1:right]
@@ -14,7 +15,7 @@ def longestPalindrome(s: str) -> str:
     if s == s[::-1] or len(s) < 2:
         return s
     result = ''
-
+    # 슬라이딩 윈도우 우측으로 이동
     for i in range(len(s) - 1):
         result = max(result,
                      expand(i, i + 1),
