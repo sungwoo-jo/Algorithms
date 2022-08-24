43a62e77852e3ae6aae6a7307353a7ea584a4c5f
sungwoo-jo <sungwoo9671@naver.com>
2022-08-24 13:03:12 +0900

    longest-palindrome-substring

---
+++ string/longest-palindrome-substring.py
@@ -0,0 +1,26 @@
+# input: "babad"
+# output: "bab"
+
+# input: "cbbd"
+# output: "bb"
+
+def longestPalindrome(s: str) -> str:
+    def expand(left: int, right: int) -> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1:right]
+
+    if s == s[::-1] or len(s) < 2:
+        return s
+    result = ''
+
+    for i in range(len(s) - 1):
+        result = max(result,
+                     expand(i, i + 1),
+                     expand(i, i + 2),
+                     key = len)
+    return result
+
+s = "babad"
+print(longestPalindrome(s))
