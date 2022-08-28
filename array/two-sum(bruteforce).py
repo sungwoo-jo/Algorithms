1e3ea46a4068e8902d1650d143b962d2e9311c7d
sungwoo-jo <sungwoo9671@naver.com>
2022-08-28 22:29:23 +0900

    two-sum

---
+++ array/two-sum(bruteforce).py
@@ -0,0 +1,12 @@
+# input: nums = [2, 7, 11, 15], target = 9
+# output: [0, 1]
+
+def twoSum(nums: list[int], target: int) -> list[int]:
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target)) 
