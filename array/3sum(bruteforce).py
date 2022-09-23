5217cab54f1ba9cff45926ff1aadcc0601203171
sungwoo-jo <sungwoo9671@naver.com>
2022-09-23 10:55:24 +0900

    3sum

---
+++ array/3sum(bruteforce).py
@@ -0,0 +1,19 @@
+def three_sum(nums: list[int]) -> list[int]:
+    nums.sort()
+    result = []
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        for j in range(i+1, len(nums) - 1):
+            if j > i + 1 and nums[j] == nums[j - 1]:
+                continue
+            for k in range(j+1, len(nums)):
+                if k > j + 1 and nums[k] == nums[k - 1]:
+                    continue
+                if nums[i] + nums[j] + nums[k] == 0:
+                    result.append([nums[i],nums[j],nums[k]])
+
+    return result
+
+nums = [-1, 0, 1, 2, -1, -4]
+print(three_sum(nums))
